801475d89476a16eb6359741d8194cb91407d7c9
Sam Tarakajian <sam@cycling74.com>
2020-02-23 21:07:51 -0500

    Using the correct sensor type

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log("Starting node sensor script");
 
 const sensor = require("node-dht-sensor");
  
-sensor.read(11, 14, function(err, temperature, humidity) {
+sensor.read(22, 14, function(err, temperature, humidity) {
   if (!err) {
     console.log(`temp: ${temperature}°C, humidity: ${humidity}%`);
   } else {
