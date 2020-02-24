ee2339a67e9ea25221d83f8970f90f99e62bb0ab
Sam Tarakajian <sam@cycling74.com>
2020-02-23 21:04:27 -0500

    Added error logging

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ const sensor = require("node-dht-sensor");
 sensor.read(11, 14, function(err, temperature, humidity) {
   if (!err) {
     console.log(`temp: ${temperature}°C, humidity: ${humidity}%`);
+  } else {
+      console.log(err);
   }
 });
